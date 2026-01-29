90a37a2df15d635487d43091beb52bdd592fcba1
shahidafridi-collab <shahidafridiktr0786@gmail.com>
2026-01-29 23:48:59 +0530

    change in tailwind config.js

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(),tailwindcss()],
-// })
-
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        serif: ['Playfair Display', 'Georgia', 'serif'], // Add a premium font
-      },
-    },
-  },
+export default defineConfig({
   plugins: [react(),tailwindcss()],
-}
+})
